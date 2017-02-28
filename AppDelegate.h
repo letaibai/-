181bdba92cc8a136d53413b39f2dcb53da510071
letaibai <letaibai@gmail.com>
2017-02-28 10:07:42 +0800

    找回版本

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  段子内涵图
+//
+//  Created by 李卫 on 2017/2/24.
+//  Copyright © 2017年 李卫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
