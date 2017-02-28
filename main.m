567224e536f535b5fad968b84921a0160113eb02
letaibai <letaibai@gmail.com>
2017-02-28 10:09:08 +0800

    阿瓦啊
    嗷嗷待食

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  段子内涵图
+//
+//  Created by 李卫 on 2017/2/24.
+//  Copyright © 2017年 李卫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
